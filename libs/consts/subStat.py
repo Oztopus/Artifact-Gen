3954515e797a8994d297244941a28c7a4a517358
Jason Tjin <jtji0003@student.monash.edu>
2023-07-31 21:47:02 +1000

    Added sub stats for flower, plume and sands

---
+++ libs/consts/subStat.py
@@ -0,0 +1,89 @@
+"""
+All distribution values were found from:
+https://genshin-impact.fandom.com/wiki/Artifact/Distribution
+
+"""
+
+flowerSubStat = [
+    "atk",  # 15.79
+    "def",  # 15.79
+    "atk_",  # 10.53
+    "def_",  # 10.53
+    "hp_",  # 10.53
+    "enerRech",  # 10.53
+    "eleMas",  # 10.53
+    "critRate_",  # 7.89
+    "critDMG_",  # 7.89
+]
+
+plumeSubStat = [
+    "hp",  # 15.79
+    "def",  # 15.79
+    "atk_",  # 10.53
+    "def_",  # 10.53
+    "hp_",  # 10.53
+    "enerRech",  # 10.53
+    "eleMas",  # 10.53
+    "critRate_",  # 7.89
+    "critDMG_",  # 7.89
+]
+
+hpSandsSubStat = [
+    "hp",  # 15
+    "atk",  # 15
+    "def",  # 15
+    "atk_",  # 10
+    "def_",  # 10
+    "enerRech",  # 10
+    "eleMas",  # 10
+    "critRate_",  # 7.5
+    "critDMG_",  # 7.5
+]
+
+atkSandsSubStat = [
+    "hp",  # 15
+    "atk",  # 15
+    "def",  # 15
+    "hp_",  # 10
+    "def_",  # 10
+    "enerRech",  # 10
+    "eleMas",  # 10
+    "critRate_",  # 7.5
+    "critDMG_",  # 7.5
+]
+
+defSandsSubStat = [
+    "hp",  # 15
+    "atk",  # 15
+    "def",  # 15
+    "hp_",  # 10
+    "atk_",  # 10
+    "enerRech",  # 10
+    "eleMas",  # 10
+    "critRate_",  # 7.5
+    "critDMG_",  # 7.5
+]
+
+erSandsSubStat = [
+    "hp",  # 15
+    "atk",  # 15
+    "def",  # 15
+    "hp_",  # 10
+    "atk_",  # 10
+    "def_",  # 10
+    "eleMas",  # 10
+    "critRate_",  # 7.5
+    "critDMG_",  # 7.5
+]
+
+emSandsSubStat = [
+    "hp",  # 15
+    "atk",  # 15
+    "def",  # 15
+    "hp_",  # 10
+    "atk_",  # 10
+    "def_",  # 10
+    "enerRech",  # 10
+    "critRate_",  # 7.5
+    "critDMG_",  # 7.5
+]
