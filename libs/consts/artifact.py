48a95612833bb337f32b192616e64cd3ca296b16
Jason Tjin <jtji0003@student.monash.edu>
2023-07-31 12:52:38 +1000

    Added 5 star artifs and main stat distribution %

---
+++ libs/consts/artifact.py
@@ -0,0 +1,43 @@
+allArtifactSetKeys = [
+    'ArchaicPetra',
+    'BlizzardStrayer',
+    'BloodstainedChivalry',
+    'CrimsonWitchOfFlames',
+    'DeepwoodMemories',
+    'DesertPavilionChronicle',
+    'EchoesOfAnOffering',
+    'EmblemOfSeveredFate',
+    'FlowerOfParadiseLost',
+    'GildedDreams',
+    'GladiatorsFinale',
+    'HeartOfDepth',
+    'HuskOfOpulentDreams',
+    'Lavawalker',
+    'MaidenBeloved',
+    'MartialArtist',
+    'NoblesseOblige',
+    'NymphsDream',
+    'OceanHuedClam',
+    'PaleFlame',
+    'RetracingBolide',
+    'ShimenawasReminiscence',
+    'TenacityOfTheMillelith',
+    'ThunderingFury',
+    'Thundersoother',
+    'VermillionHereafter',
+    'ViridescentVenerer',
+    'VourukashasGlow',
+    'WanderersTroupe'
+]
+
+allArtifactSlotKeys = [
+    'flower',
+    'plume',
+    'sands',
+    'goblet',
+    'circlet'
+]
+
+artifactLevelKey = 20
+
+artifactRarityKey = 5
---
+++ libs/consts/mainStat.py
@@ -0,0 +1,46 @@
+"""
+All distribution values were found from:
+https://genshin-impact.fandom.com/wiki/Artifact/Distribution
+
+"""
+
+flowerMainStat = [
+    "hp"  # 100%
+]
+
+plumeMainStat = [
+    "atk"  # 100%
+]
+
+sandsMainStat = [
+    "atk_",  # 26.68
+    "def_",  # 26.66
+    "hp_"  # 26.66
+    "eleMas",  # 10.00
+    "enerRech",  # 10.00
+]
+
+gobletMainStat = [
+    "atk_",  # 19.25
+    "def_",  # 19.25
+    "hp_",  # 19.00
+    "physical_dmg_",  # 5.00
+    "anemo_dmg_",  # 5.00
+    "geo_dmg_",  # 5.00
+    "electro_dmg_",  # 5.00
+    "hydro_dmg_",  # 5.00
+    "pyro_dmg_",  # 5.00
+    "cryo_dmg_",  # 5.00
+    "dendro_dmg_",  # 5.00
+    "eleMas"  # 2.50
+]
+
+circletMainStat = [
+    "atk_",  # 22
+    "def_",  # 22
+    "hp_",  # 22
+    "critRate_",  # 10
+    "critDMG_",  # 10
+    "heal_",  # 10
+    "eleMas"  # 4
+]
